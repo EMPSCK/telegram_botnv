00832b492a33f43cd2b0f345f6707a94cad88c4d
Artem <artem.bulatov.2024@bk.ru>
2024-07-25 19:07:14 +0300

    NewOb1@

--- config.py
+++
@@ -1,9 +0,0 @@
-TOKEN = '7016681556:AAEcKZ1lyOsSkgSOuS3eQvM6p7OqonHC168'
-SCRUTINEER_ID = 6887839538
-CHAIRPERSON_ID = 834140698
-
-
-host = 'server70.hosting.reg.ru'
-user = 'u1059284_remote'
-password = 'WEg1M13q8L6V'
-db_name = 'u1059284_ss6bot'
